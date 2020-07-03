bbc6a5f33c1ae3eb48233616e15282879a399451
Ravi-bit <varmaravi573@gmail.com>
2020-07-03 18:32:03 +0530

    Updates

---
+++ precache-manifest.25f992c7e10d7eff1ef3529c74a3e248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ec9c5e210026efd170a69ad877706d",
+    "url": "/Recipe-Finder-React/index.html"
+  },
+  {
+    "revision": "9931345dc62d8b4a9c9a",
+    "url": "/Recipe-Finder-React/static/css/main.6e150e2b.chunk.css"
+  },
+  {
+    "revision": "89175b5b40f018356455",
+    "url": "/Recipe-Finder-React/static/js/2.5b1f8760.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Finder-React/static/js/2.5b1f8760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9931345dc62d8b4a9c9a",
+    "url": "/Recipe-Finder-React/static/js/main.2f45b639.chunk.js"
+  },
+  {
+    "revision": "dd754edf7890d3c9f94f",
+    "url": "/Recipe-Finder-React/static/js/runtime-main.2235f8e7.js"
+  }
+]);
